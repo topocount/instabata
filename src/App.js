b11e2bff9a30795fe622a98ab3c7f65090eb1522
topocount <kevinsiegler54@gmail.com>
2018-12-31 12:37:35 -0600

    workouts generate

--- src/App.js
+++ src/App.js
@@ -11,32 +11,82 @@ class App extends Component {
         coreLib: coreExercises,
         limbLib: limbExercises
       },
-      workoutData: [
-        {key: '1', coreExercise: 'situps', limbExercise: 'pushups' },
-        {key: '2', coreExercise: 'V-Ups', limbExercise: 'Burpees' },
+      initialWorkoutData: [
+        {key: '1', coreExercise: '', limbExercise: '' },
+        {key: '2', coreExercise: '', limbExercise: '' },
         {key: '3', coreExercise: '', limbExercise: '' },
         {key: '4', coreExercise: '', limbExercise: '' },
         {key: '5', coreExercise: '', limbExercise: '' },
         {key: '6', coreExercise: '', limbExercise: '' },
         {key: '7', coreExercise: '', limbExercise: '' },
         {key: '8', coreExercise: '', limbExercise: '' },
-      ]
+      ],
+      get workoutData () {return JSON.parse(JSON.stringify(this.initialWorkoutData))}
     }
+
+    console.log(this.state.workoutLibrary)
   }
 
   workoutChange = e => {
     console.log('Value: ', e.target.value,'\nName: ', e.target.name)
     const keys = e.target.name.split(',')
     let workoutData = this.state.workoutData
-    //workoutData[keys[0]].keys[1]
     workoutData[keys[0]][keys[1]] = e.target.value
+    this.setState({workoutData})
+  }
+
+  resetWorkout = e => {
+    let workoutData = JSON.parse(JSON.stringify(this.state.initialWorkoutData))
+    this.setState({workoutData})
+  }
+
+  generateWorkout = e => {
+    console.log(this.state)
+    let workoutData = this.state.workoutData, coreExList = Object.assign([], this.state.workoutLibrary.coreLib), limbExList = Object.assign([], this.state.workoutLibrary.limbLib)
+    let currentCoreIndex = coreExList.length, currentLimbIndex = limbExList.length, temporaryValue, randomIndex
+    console.log(coreExList, limbExList)
+    while ( 0 !== currentCoreIndex ) {
+      randomIndex = Math.floor(Math.random() * currentCoreIndex)
+      currentCoreIndex -= 1
+      console.log(currentCoreIndex, coreExList.length)
+      temporaryValue = coreExList[currentCoreIndex]
+      coreExList[currentCoreIndex] = coreExList[randomIndex]
+      coreExList[randomIndex] = temporaryValue
+    }
+
+    while ( 0 !== currentLimbIndex ) {
+      randomIndex = Math.floor(Math.random() * currentLimbIndex)
+      currentLimbIndex -= 1
+      temporaryValue = limbExList[currentLimbIndex]
+      limbExList[currentLimbIndex] = limbExList[randomIndex]
+      limbExList[randomIndex] = temporaryValue
+    }
+
+    console.log(limbExList,coreExList)
+
+    workoutData.map(round => {
+      round.coreExercise = (round.coreExercise) ? round.coreExercise : coreExList.pop()
+      let existsArray = workoutData.map(rnd => (rnd.key === round.key)?'':rnd.coreExercise)
+      while (typeof(existsArray.find(el => el.toLowerCase() === round.coreExercise.toLowerCase())) !== 'undefined') {
+        round.coreExercise = coreExList.pop()
+      }
+      round.limbExercise = (round.limbExercise) ? round.limbExercise : limbExList.pop()
+      existsArray = workoutData.map(rnd => (rnd.key === round.key)?'':rnd.limbExercise)
+      while (typeof(existsArray.find(el => el.toLowerCase() === round.limbExercise.toLowerCase())) !== 'undefined') {
+        round.limbExercise = limbExList.pop()
+      }      
+      return round
+    })
+
     this.setState({workoutData})
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <input type='button' name='generateWorkoutButton' value='Generate Random Workout' onClick={this.generateWorkout} />
           <WorkoutTable workoutData={this.state.workoutData} workoutChange={this.workoutChange} />
+          <input type='button' name='resetWorkoutButton' value='Clear Workout' onClick={this.resetWorkout} />
         </header>
       </div>
     );
