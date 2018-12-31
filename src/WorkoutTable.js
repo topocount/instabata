94e063e196a769ea16d8f718ac1d2c186f43c79b
topocount <kevinsiegler54@gmail.com>
2018-12-31 14:01:47 -0600

    working build

--- src/WorkoutTable.js
+++ src/WorkoutTable.js
@@ -5,12 +5,12 @@ import Table from 'rc-table'
 class WorkoutTable extends Component {
     workoutchange = this.props.workoutChange
     columns = [
-        { title: 'Round', dataIndex: 'key', key: 'Round', width: 150 },
+        { title: 'Round', dataIndex: 'key', key: 'Round', width: 80 },
         { 
             title: 'Core',
             dataIndex: 'coreExercise',
             key: 'coreExercise',
-            width: 200,
+            width: 100,
             render(value, row, index) {
                 let inputName = String(index) + ',coreExercise'
                 return <input type='text' name={inputName} placeholder='Enter an exercise' value={value} onChange={WorkoutTable.workoutchange} />
